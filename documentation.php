2946221ecf567e9c38f5b3761b71691a0fc55081
Theo Martin <theo.martin22@icloud.com>
2016-05-12 19:47:59 +0200

    Create solid box

--- documentation.php
+++ documentation.php
@@ -1,11 +1,76 @@
 <?php include "header.php"; ?>
 <?php include "sidebar.php"; ?>
-	<div class="content-wrapper">
-		<section class="content-header">
-			<h1>Dashboard <small>Documentation</small></h1>
-		</section>
-		<section class="content">
-			<!-- Contenu ici : titre h2 formater -->
-		</section>
-	</div>
+<div class="content-wrapper">
+	<section class="content-header">
+		<h1>Dashboard <small>Documentation</small></h1>
+	</section>
+	<section class="content">
+		<h2>Boxes</h2>
+		<h3>Solid Box</h3>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						Panel Default
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-green">
+					<div class="panel-heading">
+						Panel Green
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-red">
+					<div class="panel-heading">
+						Panel Red
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="panel panel-yellow">
+					<div class="panel-heading">
+						Panel Yellow
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						Panel Primary
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="panel panel-blue">
+					<div class="panel-heading">
+						Panel Blue
+					</div>
+					<div class="panel-body">
+						Panel content
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
 <?php include "footer.php"; ?>
