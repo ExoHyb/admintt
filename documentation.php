c4220656c334fda6f0807f3fbd374acd8f0090c1
Theo Martin <theo.martin22@icloud.com>
2016-05-13 11:56:01 +0200

    display html in html + colors

--- documentation.php
+++ documentation.php
@@ -136,6 +136,24 @@
 				</div>
 			</div>
 		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<figure>
+					<pre>
+						<code id="margin-code">
+							<span class="nt">&lt;div</span><span class="na"> class=</span><span class="s">"panel box-blue"</span><span class="nt">&gt;</span>
+							<span class="nt">&lt;div</span><span class="na"> class=</span><span class="s">"panel-heading"</span><span class="nt">&gt;</span>
+							<span class="nt">&lt;p&gt;</span>Titre de la box<span class="nt">&lt;/p&gt;</span>
+							<span class="nt">&lt;/div&gt;</span>
+							<span class="nt">&lt;div</span><span class="na"> class=</span><span class="s">"panel-body"</span><span class="nt">&gt;</span>
+							<span class="nt">&lt;p&gt;</span>Contenu de la box<span class="nt">&lt;/p&gt;</span>
+							<span class="nt">&lt;/div&gt;</span>
+							<span class="nt">&lt;/div&gt;</span>
+						</code>
+					</pre>
+				</figure>
+			</div>
+		</div>
 	</section>
 </div>
 <?php include "footer.php"; ?>
--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
-	</div> <!-- fermeture div wrapper dans header.php -->
 </body>
 <script src="bower_components/jquery/dist/jquery.min.js"></script>
 <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
